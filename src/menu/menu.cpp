11c7191ef8b0410ac772f64fc06b29d3dc7290c9
Eliasin <steven.pham.8.2000@gmail.com>
2017-05-17 15:42:46 -0400

    Add menu implementation

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -1,5 +1,23 @@
 /*
-* Created by Mark on 5/17/2017.
-*/
+ * Created by Mark on 5/17/2017.
+ */
 
 #include "menu.h"
+
+namespace menu {
+
+	void Menu::forEach(const std::function<void(const Menu::IndexType, const MenuChoice&)> &consumer) const noexcept {
+		for (Menu::IndexType i = 0; i < menuChoices.size(); i++) {
+			consumer(i, *menuChoices[i]);
+		}
+	}
+
+	const Menu::IndexType Menu::size() const noexcept {
+		return menuChoices.size();
+	}
+
+	const MenuChoice& Menu::at(const Menu::IndexType index) const noexcept {
+		return *menuChoices[index];
+	}
+
+}
