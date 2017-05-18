578802e717c8d4cf2c2224fe9fa8e973a2a15d60
Eliasin <steven.pham.8.2000@gmail.com>
2017-05-18 18:46:01 -0400

    Add read from file menu option

---
+++ src/menu/readSystemsFromFile.cpp
@@ -0,0 +1,93 @@
+#include "readSystemsFromFile.hpp"
+
+#include <fstream.h>
+#include <iostream>
+
+#include "linsys/parser.h"
+
+namespace menu {
+
+	template <typename Iterator, typename Parser>
+		const static bool parseLinearEquationReturningResult(linsys::LinearEquation& equation, const Parser& parser, const Iterator&& begin, const Iterator&& end) {
+			using namespace boost::spirit;
+
+			const bool parseStatus = qi::phrase_parse(begin, end, parser, ascii::space, equation);
+
+			return parseStatus;
+		}
+
+	static void skipUntilNextLinearEquation(std::istream& inputStream) {
+		std::string line;
+		for (; std::getline(inputStream, line);) {
+			if (line.size() == 0 || line.at(0) == 13) break;
+		}
+	}
+
+	template <typename Parser>
+		void readLinearEquationsFromStream(std::istream&& input, linsys::SessionEnvironment& session, const Parser& parser) {
+			while (input) {
+				std::string firstEquationText;
+				if (!std::getline(input, firstEquationText)) {
+					skipUntilNextLinearEquation(input);
+					continue;
+				}
+
+				std::string secondEquationText;
+				if (!std::getline(input, secondEquationText)) {
+					skipUntilNextLinearEquation(input);
+					continue;
+				}
+
+				linsys::LinearEquation firstEquation;
+				const bool firstEqStatus = parseLinearEquationReturningResult(firstEquation, parser, std::cbegin(firstEquationText), std::cend(firstEquationText));
+
+				if (!firstEqStatus) {
+					skipUntilNextLinearEquation(input);
+					continue;
+				}
+
+				linsys::LinearEquation secondEquation;
+				const bool secondEqStatus = parseLinearEquationReturningResult(secondEquation, parser, std::cbegin(secondEquationText), std::cend(secondEquationText));
+
+				if (!secondEqStatus) {
+					skipUntilNextLinearEquation(input);
+					continue;
+				}
+
+				try {
+					const linsys::LinearSystem sys = linsys::LinearSystem::of(std::move(firstEquation), std::move(secondEquation));
+					session.addSystem(std::move(sys));
+				} catch (...) {}
+
+				std::string supposedEmptyText;
+				if (!std::getline(input, supposedEmptyText)) {
+					break;
+				} else if (supposedEmptyText.size() != 0 && supposedEmptyText.at(0) != 13) {
+					//            Careful for the carriage return on Windows. This might be 1 not 0 if reading a windows file
+					break;
+				}
+			}
+		}
+
+	void ReadSystemsFromFileMenuChoice::select() {
+		std::ifstream file;
+		while (true) {
+			std::string input;
+
+			std::cout << "Please enter the name of the file: " << std::endl;
+			std::cin >> input;
+			file.open(input);
+
+			if (file) break;
+
+			std::cout << "File could not be found, please try again." << std::endl;
+			waitForEnterKey();
+			clearScreen();
+		}
+
+		LinearEquationParser linearEquationParser;
+		readLinearEquationsFromStream(std::move(file), session, linearEquationParser);
+		std::cout << "There are now " << session.size() << " systems." << std::endl;
+	}
+
+}
---
+++ src/menu/readSystemsFromFile.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "menu.h"
+
+#include "linsys/session.h"
+
+namespace menu {
+
+	class ReadSystemsFromFileMenuChoice : public MenuChoice {
+		linsys::SessionEnvironment& session;
+	public:
+		void select();
+	}
+
+}
