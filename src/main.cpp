9f80a0d17d40c6114763a0848bb8eaf47b6c1d6a
Eliasin <steven.pham.8.2000@gmail.com>
2017-05-19 14:22:11 -0400

    Add solve systems menu option

--- src/main.cpp
+++ src/main.cpp
@@ -15,41 +15,43 @@
 #include "menu/displayMenuOption.hpp"
 #include "menu/quitMenuOption.hpp"
 #include "menu/readSystemsFromFile.hpp"
+#include "menu/deferringmenuoption.h"
 #include "menu/getSystemFromUserMenuOption.hpp"
 
 int main() {
 	linsys::SessionEnvironment session;
 
 	const menu::Menu menu {
-            new menu::ReadSystemsFromFileMenuChoice(session),
-            new menu::CreateRandomSystemsMenuChoice(session),
-            new menu::GetSystemFromUserMenuChoice(session),
+		new menu::ReadSystemsFromFileMenuChoice(session),
+			new menu::CreateRandomSystemsMenuChoice(session),
+			new menu::GetSystemFromUserMenuChoice(session),
 			new menu::ChallengeUserMenuChoice(session),
-            new menu::DisplayMenuChoice(session),
+			new menu::DeferringMenuChoice("Solve All Systems", [](){std::cout << "All systems solved!" << std::endl; }),
+			new menu::DisplayMenuChoice(session),
 			new menu::QuitMenuChoice()
 	};
 
-    for (;;) {
-        menu::clearScreen();
-        menu.forEach([&](const menu::Menu::IndexType i, const menu::MenuChoice &choice) {
-            std::cout << i << ": " << choice.getName() << std::endl;
-        });
+	for (;;) {
+		menu::clearScreen();
+		menu.forEach([&](const menu::Menu::IndexType i, const menu::MenuChoice &choice) {
+				std::cout << i << ": " << choice.getName() << std::endl;
+				});
 
-        std::cout << "Enter an option: ";
-        int input;
-        if (!(std::cin >> input)) {
-            std::cout << "Illegal input: must be a number between 1 and " << menu.size() << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            menu::waitForEnterKey();
-        } else {
-            if (input >= 1 && input <= menu.size()) {
-                menu.at(input).select();
-            } else {
-                std::cout << "Illegal option '" << input << "': Must be between 1 and " << menu.size() << std::endl;
-            }
-        }
-        menu::waitForEnterKey();
-    }
+		std::cout << "Enter an option: ";
+		int input;
+		if (!(std::cin >> input)) {
+			std::cout << "Illegal input: must be a number between 1 and " << menu.size() << std::endl;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			menu::waitForEnterKey();
+		} else {
+			if (input >= 1 && input <= menu.size()) {
+				menu.at(input).select();
+			} else {
+				std::cout << "Illegal option '" << input << "': Must be between 1 and " << menu.size() << std::endl;
+			}
+		}
+		menu::waitForEnterKey();
+	}
 	return 0;
 }
