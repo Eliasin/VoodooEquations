79a2c535596ef42496c2e5aaa4160154983377b1
foundry27 <xfoundry27@yahoo.ca>
2017-05-19 14:34:00 -0400

    Extract the linear system answer parser into a system solution parsing utility

---
+++ src/linsys/systemSolutionParsingUtility.h
@@ -0,0 +1,32 @@
+/*
+* Created by Mark on 5/19/2017.
+*/
+
+#pragma once
+
+#include <string>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/rational.hpp>
+
+#include "systemparts.h"
+
+namespace linsys {
+    namespace {
+        namespace qi = boost::spirit::qi;
+
+        qi::rule<std::string::const_iterator, boost::rational<int>()> fraction = qi::int_
+                >> ('/' >> qi::int_ | qi::attr(1));
+        qi::rule<std::string::const_iterator, linsys::LinearSystemSolution()> systemSolution = qi::char_('(')
+                >> fraction >> qi::char_(',') >> fraction
+                >> qi::char_(')');
+    }
+
+    template <typename Iterator>
+    const bool parseLinearSystemSolutionReturningResult(
+            LinearSystemSolution& solution, const Iterator&& begin, const Iterator&& end) {
+        const bool parseStatus = ::boost::spirit::qi::phrase_parse(begin, end, systemSolution, ::boost::spirit::ascii::space, solution);
+
+        return parseStatus;
+    }
+}
--- src/menu/challengeUserMenuOption.hpp
+++ src/menu/challengeUserMenuOption.hpp
@@ -12,20 +12,14 @@
 #include "linsys/session.h"
 #include "linsys/parser.h"
 #include "linsys/systemparts.h"
+#include "linsys/systemSolutionParsingUtility.h"
 
 namespace menu {
 
 	class ChallengeUserMenuChoice : public MenuChoice {
 	public:
 		ChallengeUserMenuChoice(const linsys::SessionEnvironment& session)
-			: MenuChoice("Challenge User"), session(session), rng(std::random_device()())
-		{
-			namespace qi = boost::spirit::qi;
-
-			fraction = qi::int_	>> ('/' >> qi::int_ | qi::attr(1));
-
-			systemSolution = qi::char_('(') >> fraction >> qi::char_(',') >> fraction >> qi::char_(')');
-		}
+			: MenuChoice("Challenge User"), session(session), rng(std::random_device()()) {}
 
 		void select() const override {
 			if (!session.size()) {
@@ -44,15 +38,17 @@ namespace menu {
 			for (;;) {
 				std::cout << "Take a shot at finding the solution in the form '(x, y)' or type 'none' for no solution: ";
 				std::cin >> input;
-				if (input == "none" && !system.solution) {
-					std::cout << "You got the correct solution! Congratulations!" << std::endl;
-				} else {
-					std::cout << "You got the wrong solution! There was none!" << std::endl;
-					waitForEnterKey();
-					return;
-				}
-				const bool wasParseSuccess = boost::spirit::qi::phrase_parse(std::cbegin(input), std::cend(input), systemSolution,
-															   boost::spirit::ascii::space, equation);
+                if (input == "none") {
+                    if (!system.solution) {
+                        std::cout << "You got the correct solution! Congratulations!" << std::endl;
+                    } else {
+                        std::cout << "You got the wrong solution! There was none!" << std::endl;
+                        waitForEnterKey();
+                        return;
+                    }
+                }
+
+				const bool wasParseSuccess = linsys::parseLinearSystemSolutionReturningResult(equation, std::cbegin(input), std::cend(input));
 				if (wasParseSuccess) {
 					break;
 				} else {
@@ -71,8 +67,6 @@ namespace menu {
 
 	private:
 		const linsys::SessionEnvironment& session;
-		boost::spirit::qi::rule<std::string::const_iterator, boost::rational<int>()> fraction;
-		boost::spirit::qi::rule<std::string::const_iterator, linsys::LinearSystemSolution()> systemSolution;
 		mutable std::mt19937 rng;
 	};
 
